ee2c716b4fdd20a32194ed10d8983f556bd8b412
erenyusufduran <eren.duran@colins.com>
2023-10-14 21:34:26 +0300

    phoneNumber for wp

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FloatingWhatsApp
-      phoneNumber="+905325132858"
+      phoneNumber="+905339302927"
       accountName="Birey Tour"
       avatar="/logo50x50.png"
       statusMessage="En fazla 2 saat içerisinde geri dönüş yapacağız."
